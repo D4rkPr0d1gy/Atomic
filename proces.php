44d363b11ceac1b74383d2948c46366ca6bf8e96
D4rkPr0d1gy <45219039+D4rkPr0d1gy@users.noreply.github.com>
2020-04-01 22:09:32 -0600

    Add files via upload

--- proces.php
+++ proces.php
@@ -4,7 +4,7 @@ session_start();
 
 $server = 'localhost';
 $username = 'root';
-$password = 'TheDarkProdigy78'; //Aquí va la contraseña de su servidor
+$password = ''; //Aquí va la contraseña de su servidor
 $database = 'login_database'; /* cambie por el nombre de su BD debe ser creada la tabla con 
 los siguientes datos 1.- ID tipo INT con un autoincrement Primary 2.- email tipo varchar de 200 3.-password tipo
 varchar de 200     */
